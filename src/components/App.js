e9d06cc412e83fc921853eecd76fb94c32d7fd07
jeff.smith <slightlyoffbass@gmail.com>
2019-10-07 15:46:14 -0500

    add icons

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,23 @@ import React from 'react';
 
 class App extends React.Component {
 
+    state = {language: 'english'};
+
+    onLanguageChange = language =>{
+        this.setState({language});
+    }
+
     render(){
-        return <div>App</div>;
+        return (
+            <div className="ui container">
+                <div>
+                    Select a language 
+                    <i className="flag us" onClick={()=> this.onLanguageChange('english')}/>
+                    <i className="flag nl"  onClick={()=> this.onLanguageChange('dutch')}/>
+                </div>
+                {this.state.language}
+            </div>
+        )
     }
 
     
